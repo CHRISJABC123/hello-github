8ac11d8fd9714a0c4fa72f83f98382e38d905b5b
typingJ <253278736@qq.com>
2021-01-15 13:13:11 +0800

    this is third edit

--- test01/src/com/chrisj/testgithub/Test.java
+++ test01/src/com/chrisj/testgithub/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello GitHub");
         System.out.println("second");
+        System.out.println("third");
     }
 }
