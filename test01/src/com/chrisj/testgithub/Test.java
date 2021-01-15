b059645dd938271208e7a934c28d8721b88a0d48
typingJ <253278736@qq.com>
2021-01-15 11:50:24 +0800

    this is second edit

--- test01/src/com/chrisj/testgithub/Test.java
+++ test01/src/com/chrisj/testgithub/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello GitHub");
         System.out.println("this is firs edit");
+        System.out.println("this is second edit");
     }
 }
