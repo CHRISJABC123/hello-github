4418dab836b29a423f7c1b3c4af289cec2b1adc5
typingJ <253278736@qq.com>
2021-01-15 11:16:35 +0800

    this is a project for test

---
+++ test01/src/com/chrisj/testgithub/Test.java
@@ -0,0 +1,11 @@
+package com.chrisj.testgithub;
+
+/**
+ * chrisJ
+ * 2021/1/15
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello GitHub");
+    }
+}
