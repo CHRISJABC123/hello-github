7f0107c45db14587eb5b41ba1ba62dd1f954412d
typingJ <253278736@qq.com>
2021-01-15 13:55:28 +0800

    edit

--- test01/src/com/chrisj/testgithub/Test.java
+++ test01/src/com/chrisj/testgithub/Test.java
@@ -7,9 +7,5 @@ package com.chrisj.testgithub;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello GitHub");
-        System.out.println("second");
-        System.out.println("third");
-        System.out.println("use pull request");
-        System.out.println("four edit");
     }
 }
