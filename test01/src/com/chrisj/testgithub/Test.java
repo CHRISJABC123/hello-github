e733cef3ec740b116da9178889b866d013aedd6a
jidongxing <253278736@qq.com>
2021-03-04 09:25:38 +0800

    第二次提交

--- test01/src/com/chrisj/testgithub/Test.java
+++ test01/src/com/chrisj/testgithub/Test.java
@@ -8,6 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello GitHub");
         System.out.println("hello ");
-		System.out.println("BASHÉÏ´«");
+		System.out.println("BASHä¸Šä¼ ");
     }
 }
